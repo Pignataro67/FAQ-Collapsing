7b8984c6fe96eb13edcbd38c6091b6937758e6d7
Pignataro67 <Pignataro67@gmail.com>
2022-12-13 19:43:53 -0800

    JS

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ render() {
           onChange={this.handleDropdownChange} 
           placeholder={this.props.label} 
           search selection 
-        />
+          onFocus={this.onFocus}
+          options={this.state.suggestedLocations} 
+          open={this.state.isDropdownOpen || false} 
+          icon={<span></span>}
+/>
         <Button icon>
           <Icon name='search'/> 
