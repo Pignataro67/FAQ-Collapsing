32b008124c945d6e1afb9d2cec103549bda67d49
Pignataro67 <Pignataro67@gmail.com>
2022-12-13 19:43:38 -0800

    JS

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ render() {
         <Dropdown
           loading={this.props.loading} 
           onSearchChange={this.props.onChange} 
-          
+          onChange={this.handleDropdownChange} 
+          placeholder={this.props.label} 
+          search selection 
+        />
         <Button icon>
           <Icon name='search'/> 
